aade4e7b85bfd0ae1fe5c1d39c554132a021822f
white bear <pavlo.shui2006@gmail.com>
2021-05-06 17:00:36 +0300

    somescode

---
+++ somecode.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int abc = 0, x = 0;
+    while (x<=360*300000*555146418648){
+        cout << x <<"\n";
+        x+=1;
+
+
+        if (x==-1)
+{
+        cout <<"time is out\n";
+        abc = 2021;
+        cout <<"HAPPY NEW YEAR!!!"<< abc<<"\n";
+}
+}
+
+
+}
