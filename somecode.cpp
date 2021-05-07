4275bfe0b8605953a556bb41ba6a1a76ea05118a
white bear <pavlo.shui2006@gmail.com>
2021-05-07 17:34:26 +0300

    DEBUG

--- somecode.cpp
+++ somecode.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     int abc = 0, x = 0;
-    while (x<=360*300000*555146418648){
+    while (x<=5000){
         cout << x <<"\n";
         x+=1;
 
 
-        if (x==-1)
+        if (x==5000)
 {
         cout <<"time is out\n";
         abc = 2021;
